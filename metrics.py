705c3a60f9f96e0fc35b2f6d5f45ae9161e38b18
Azen0n <jack.2013.ghost@gmail.com>
2021-11-13 13:54:54 +0500

    Add students_rate metric and call_all_metrics function

--- metrics.py
+++ metrics.py
@@ -1,9 +1,8 @@
 import numpy as np
 
 
-# Метрики
-# 1. Квадрат отклонения приоритета
 def squared_priority_deviation(students):
+    """Квадрат отклонения приоритета."""
     deviation = 0
     for student in students:
         if student.elective_id is None:
@@ -14,8 +13,8 @@ def squared_priority_deviation(students):
     return deviation / len(students)
 
 
-# 2. Среднее приоритета
 def mean_priority(students):
+    """Среднее приоритета."""
     deviation = 0
     for student in students:
         if student.elective_id is None:
@@ -26,8 +25,8 @@ def mean_priority(students):
     return deviation / len(students)
 
 
-# 3. Сумма произведения приоритета на рейтинг
 def sum_of_priority_performance_product(students):
+    """Сумма произведения приоритета на рейтинг."""
     deviation = 0
     for student in students:
         if student.elective_id is None:
@@ -36,3 +35,25 @@ def sum_of_priority_performance_product(students):
             index, = np.where(student.priorities == student.elective_id)
         deviation += index * student.performance
     return deviation
+
+
+def students_rate(students):
+    """Процент студентов, попавших на каждый приоритет (6 — студент не распределен)."""
+    rates = {1: 0.0, 2: 0.0, 3: 0.0, 4: 0.0, 5: 0.0, 6: 0.0}
+    for student in students:
+        if student.is_available:
+            rates[6] += 1
+        else:
+            rates[np.where(student.priorities == student.elective_id)[0][0] + 1] += 1
+
+    for key in rates:
+        rates[key] = rates[key] / len(students) * 100.0
+
+    return rates
+
+
+def call_all_metrics(students):
+    print(f'squared_priority_deviation = {squared_priority_deviation(students)}')
+    print(f'mean_priority = {mean_priority(students)}')
+    print(f'sum_of_priority_performance_product = {sum_of_priority_performance_product(students)}')
+    print(f'students_rate = {students_rate(students)}')
