9a79fadd09259a43f47afef8c1d4380e0b377408
Jegern <severuanov2001@mail.ru>
2022-01-09 14:22:01 +0500

    Добавлена кнопка ClipboardButton

--- components.py
+++ components.py
@@ -1,12 +1,14 @@
 from kivy.core.window import Window
-from kivy.properties import ObjectProperty
 from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.recycleview.views import RecycleKVIDsDataViewBehavior
-from kivymd.uix.behaviors import HoverBehavior, RectangularRippleBehavior, RoundedRectangularElevationBehavior
+from kivymd.uix.behaviors import (HoverBehavior,
+                                  RectangularRippleBehavior,
+                                  RoundedRectangularElevationBehavior,
+                                  CircularRippleBehavior)
 
 
 class HoverButton(Button, HoverBehavior):
@@ -19,12 +21,27 @@ class HoverButton(Button, HoverBehavior):
         self.background_color[3] += 0.2
 
 
-class ElectiveTextInput(TextInput):
+class ElectiveCardTextInput(TextInput):
     max_length = 50
 
     def insert_text(self, substring, from_undo=False):
         if len(self.text) < self.max_length:
             return super().insert_text(substring, from_undo=from_undo)
+
+
+class ClipboardButton(
+    HoverButton,
+    RectangularRippleBehavior,
+    RoundedRectangularElevationBehavior
+):
+    @staticmethod
+    def on_enter(**kwargs):
+        Window.set_system_cursor('hand')
+
+    @staticmethod
+    def on_leave(**kwargs):
+        Window.set_system_cursor('arrow')
+
 
 class RippleElevationHoverButton(
     HoverButton,
@@ -32,11 +49,24 @@ class RippleElevationHoverButton(
     RoundedRectangularElevationBehavior
 ):
     @staticmethod
-    def on_enter():
+    def on_enter(**kwargs):
         Window.set_system_cursor('hand')
 
     @staticmethod
-    def on_leave():
+    def on_leave(**kwargs):
+        Window.set_system_cursor('arrow')
+
+
+class ImageButton(ButtonBehavior,
+                  Image,
+                  HoverBehavior,
+                  CircularRippleBehavior):
+    @staticmethod
+    def on_enter(**kwargs):
+        Window.set_system_cursor('hand')
+
+    @staticmethod
+    def on_leave(**kwargs):
         Window.set_system_cursor('arrow')
 
 
@@ -44,6 +74,7 @@ class ListLine(RecycleKVIDsDataViewBehavior, BoxLayout):
     @staticmethod
     def button_click(button):
         button.root.line_button_callback(button)
+
 
 # FIXME: Объединить с ListLine
 class ListLine2(RecycleKVIDsDataViewBehavior, BoxLayout):
