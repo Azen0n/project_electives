c8bd896f4df6253d165e4f521ae3aaa492320ecd
Azen0n <jack.2013.ghost@gmail.com>
2022-01-24 01:22:35 +0500

    Rename function get_elective_position

--- algorithm_rework/objects.py
+++ algorithm_rework/objects.py
@@ -8,7 +8,7 @@ class Student:
     priorities: list[int]
     elective_id: int = None
 
-    def get_elective_priority(self, elective_id: int) -> int:
+    def get_elective_position(self, elective_id: int) -> int:
         """Возвращает позицию электива в списке приоритетов. Если электив не выбран, возвращает 6."""
         return self.priorities.index(elective_id) + 1 if elective_id in self.priorities else 6
 
@@ -39,15 +39,15 @@ class Elective:
         for student in students:
             if self.id in student.priorities:
                 self.students.append(student)
-                if student.get_elective_priority(self.id) == 1:
+                if student.get_elective_position(self.id) == 1:
                     self.reserve += 1
 
     def sort_students_by_priority(self, smart_students_first: bool = True):
         """Сортирует список студентов по приоритету электива и успеваемости."""
         if smart_students_first:
-            self.students.sort(key=lambda student: (student.get_elective_priority(self.id), 5 - student.performance))
+            self.students.sort(key=lambda student: (student.get_elective_position(self.id), 5 - student.performance))
         else:
-            self.students.sort(key=lambda student: (student.get_elective_priority(self.id), student.performance))
+            self.students.sort(key=lambda student: (student.get_elective_position(self.id), student.performance))
 
     def is_potentially_completable(self) -> bool:
         return self.min_capacity / 2 <= self.reserve < self.min_capacity
